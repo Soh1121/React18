ba2e377ebc956e3fbdc430e12dbd623fffb0d2b3
Soh1121 <satou.shg@gmail.com>
2025-09-04 06:31:38 +0900

    ✨feat: 準備

--- react18-explanation-react17/src/App.tsx
+++ react18-explanation-react17/src/App.tsx
@@ -1,14 +1,8 @@
-import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  useEffect(() => {
-    console.log('useEffect!!');
-  }, []);
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
