4c1ba9ff2d204b18eccc8b9ee967adc5488d054a
Soh1121 <satou.shg@gmail.com>
2025-09-17 06:35:53 +0900

    ✨feat: Suspense と Transition の組み合わせを学習

--- react18-explanation-react18/src/components/ReactQuery.tsx
+++ react18-explanation-react18/src/components/ReactQuery.tsx
@@ -2,21 +2,48 @@ import { Sidebar } from "./Sidebar";
 import { AlbumList } from "./AlbumList";
 import { TodoList } from "./TodoList";
 import { ErrorBoundary } from "react-error-boundary";
-import { Suspense } from "react";
+import { Suspense, useState, useTransition } from "react";
+
+type Tabs = "todo" | "album";
 
 export const ReactQuery = () => {
+    const [selectedTab, setSelectedTab] = useState<Tabs>("todo")
+    const [isPending, startTransition] = useTransition();
+
+    const buttonStyle = {
+        padding: "12px",
+        fontSize: "16px",
+        border: "none",
+        opacity: isPending ? 0.5 : 1,
+    };
+
+    const albumButtonStyle = {
+        ...buttonStyle,
+        backgroundColor: selectedTab === "album" ? "royalblue" : "white",
+        color: selectedTab === "album" ? "white" : "black",
+    };
+
+        const todoButtonStyle = {
+        ...buttonStyle,
+        backgroundColor: selectedTab === "todo" ? "royalblue" : "white",
+        color: selectedTab === "todo" ? "white" : "black",
+    };
+
+    const onClickTabButton = (tab: Tabs) => {
+        startTransition(() => {
+            setSelectedTab(tab);
+        });
+    };
+
     return (
         <div style={{ display: "flex", padding: "16px" }}>
             <Sidebar />
             <div style={{ flexGrow: 1 }}>
+                <button type="button" style={todoButtonStyle} onClick={() => onClickTabButton("todo")}>Todo</button>
+                <button type="button" style={albumButtonStyle} onClick={() => onClickTabButton("album")}>Album</button>
                 <ErrorBoundary fallback={<p>AlbumListエラーです！</p>}>
                     <Suspense fallback={<p>AlbumListローディング中だよ〜</p>}>
-                        <AlbumList />
-                    </Suspense>
-                </ErrorBoundary>
-                <ErrorBoundary fallback={<p>TodoListエラーです！</p>}>
-                    <Suspense fallback={<p>TodoListローディング中だよ〜</p>}>
-                        <TodoList />
+                        {selectedTab === "todo" ? <TodoList /> : <AlbumList />}
                     </Suspense>
                 </ErrorBoundary>
             </div>
